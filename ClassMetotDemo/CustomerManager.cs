f5cc91c663cb826e78e9c1f54dce9520c5257a83
mtarikg <mtarikgol97@gmail.com>
2021-03-03 23:35:14 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customerList = new List<Customer>();
+
+        public void AddCustomer(Customer customer)
+        {
+            customerList.Add(customer);
+            Console.WriteLine("Customer is added successfully.");
+            Console.WriteLine();
+        }
+
+        public void ShowCustomers()
+        {
+            Console.WriteLine("Here is all customers' information: ");
+            foreach (Customer customer in customerList)
+            {
+                Console.WriteLine("Customer ID: " + customer.CustomerID);
+                Console.WriteLine("Customer Name: " + customer.CustomerName);
+                Console.WriteLine("Customer Surname: " + customer.CustomerSurname);
+                Console.WriteLine("Customer Age: " + customer.CustomerAge);
+                Console.WriteLine();
+            }
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer " + customer.CustomerName + " " + customer.CustomerSurname + " is deleted successfully.");
+            customerList.Remove(customer);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager cm = new CustomerManager();
+
+            Customer customer1 = new Customer();
+            customer1.CustomerID = 1;
+            customer1.CustomerName = "Tarık";
+            customer1.CustomerSurname = "Göl";
+            customer1.CustomerAge = 23;
+
+            cm.AddCustomer(customer1);
+
+            Customer customer2 = new Customer();
+            customer2.CustomerID = 2;
+            customer2.CustomerName = "Ahmet";
+            customer2.CustomerSurname = "Bilgin";
+            customer2.CustomerAge = 34;
+
+            cm.AddCustomer(customer2);
+
+            Customer customer3 = new Customer();
+            customer3.CustomerID = 3;
+            customer3.CustomerName = "Mehmet";
+            customer3.CustomerSurname = "Bozkurt";
+            customer3.CustomerAge = 29;
+
+            cm.AddCustomer(customer3);
+
+            cm.ShowCustomers();
+
+            cm.DeleteCustomer(customer2);
+
+            Console.WriteLine("After deletion:");
+            cm.ShowCustomers();
+        }
+    }
+}
